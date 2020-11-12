a76cb3515d51a4d7ecfb7c78f401e85fe7bdbb6a
Hiromu <hiromu@Hiromu-3.local>
2020-11-12 23:58:00 +0900

    re push

--- chapter3/problem3.6.py
+++ chapter3/problem3.6.py
@@ -1,4 +1,32 @@
-# Q. 整数K, Nが与えられる。0 <= (X, Y, Z) <= K かつ x + y + z = NとなるK, Nは何通りあるか？O(N^2)で。
+# Q. 整数K, Nが与えられる。0 <= (X, Y, Z) <= K で、 X + Y + Z = Nとなる(X,Y,Z)は何通りあるか？O(N^2)で。
 
+import numpy as np
 
+print("The number of integers(from 0): ")
+N = int(input())
+print("Expected total: ")
+W = int(input())
 
+a = np.arange(N)
+exist = 0
+
+# 0以上K以下の整数から３個選び、その総和がWとなるのは？
+for bit in np.arange(1 << N):
+    bin_num = bin(bit)[2:]
+    count = 0
+    for b in bin_num:
+        count += int(b)
+    total = 0
+    # ２進数にしたとき、１が3個あれば、整数が３個選ばれていると言うことだから、
+    if count == 3:
+        i = 0
+        for v in a:
+            # 選んだ３つの整数の総和をとり、
+            if bit & (1 << i):
+                total += v
+            i += 1 
+    # W杜等しいくなるか？
+    if total == W:
+        exist += 1
+
+print(exist)
