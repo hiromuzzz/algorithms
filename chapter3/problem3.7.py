300e3364e930a9e0050f97dfccb2e56bf7684e37
Hiromu <hiromu@Hiromu-3.local>
2020-11-15 22:20:38 +0900

    add problem3.7

---
+++ chapter3/problem3.7.py
@@ -0,0 +1,31 @@
+# Q. 1~9の整数のみで作られた長さNの文字列Sが与えられる時、文字の間に＋を入れた時の総和を求める。
+# ex) "1,2,3" = (1+2+3) + (1+23) + (12+3) + 123 = 168
+
+import numpy as np
+
+print("The number of integers(from 0): ")
+S = input()
+
+total = 0
+
+# ＋の位置をbit演算する
+plus = len(S) - 1
+for p in np.arange(1 << plus):
+    plus_position = list(bin(p)[2:])
+    plus_position.reverse()
+    index = [i for i, x in enumerate(plus_position) if x == '1']
+    print(index)
+    if not index:
+        # ＋がない時
+        total += int(S)
+    else:
+        start = 0
+        # ＋がはいる場所で分割
+        for i in index:
+            # ＋より手前を足す
+            total += int(S[start:i+1])
+            start = i + 1
+        # 最後の＋より後ろに残っている値を足す
+        total += int(S[start:])
+
+print(total)
